956a8e51948bf09ac469de798ca7d02812599434
Andreas Johansson <andreas@ibuypink.com>
2012-04-01 22:01:24 +0200

    Support for active item
    Added full support for active item in the navigation.

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -5,52 +5,50 @@
 */
 (function($) {
 	$.fn.carouselFade = function(options) {
-	    var defaults = {  
-	    	nav: 'all',
-	        control: '#billboard-nav',
-	        transtime: 1000,
-	        fadeOut: 'fade-out',
-	        fadeIn: 'fade-in',
-	        active: 'active',
-	        delay: 500
-	    };  
-	    var options = $.extend(defaults, options); 
-	    var cCount = 0;
-	    var cRunning = false; // if animation is running or not
+		var defaults = {  
+			nav: 'all',
+			control: '#billboard-nav',
+			transtime: 1000,
+			fadeOut: 'fade-out',
+			fadeIn: 'fade-in',
+			active: 'active',
+			delay: 500
+		};  
+		var options = $.extend(defaults, options); 
+		var cCount = 0;
+		var cRunning = false; // if animation is running or not
 		var formula = 'add'; // default is add
-	    var cInt; // carousel interval; autostart
-	    var refItems = new Array(); // reference to each slide
+		var cInt; // carousel interval; autostart
+		var refItems = new Array(); // reference to each slide
 		var refCont = $(this); // container holding all slides
 		var refWidth = refCont.width();
 		var refControl = setupNav(options.nav);
 		var refCSSTrans = Modernizr.csstransitions; // CSS Transitions availability in browser
 	    return this.each(function() {  
 			refCont.find('.item').each(function(i) { 
-	            var obj = $(this)
-	            	.addClass('item-'+(i+1))
-	            	.attr({style:'transition-duration:'+options.transtime+'ms'
-	            		+'; -moz-transition-duration:'+options.transtime+'ms'
-	            		+'; -webkit-transition-duration:'+options.transtime+'ms'
-	            		+'; -o-transition-duration:'+options.transtime+'ms'});
-	            refItems[i] = obj;
-	        });
-	        $(options.control).find('a').on('click', function() {
-	        	var t = $(this);
-	            if(!cRunning) {
-	            	runCarousel({
-		            	rel:t.attr('rel'),
-		            	href:t.attr('href')
-	            	});
-	            	if(t.attr('rel')==='episode') {
-	            		t.addClass(options.active).parent().siblings().find('a').removeClass(options.active);
-	            	}
-	            }
-	            clearInterval(cInt);
-	            return false;
-	        });
-	        refCont.find(':first-child').addClass('fade-in');
-	        cInt = setInterval(init, options.transtime + options.delay);
-	    });
+			var obj = $(this)
+				.addClass('item-'+(i+1))
+				.attr({style:'transition-duration:'+options.transtime+'ms'
+					+'; -moz-transition-duration:'+options.transtime+'ms'
+					+'; -webkit-transition-duration:'+options.transtime+'ms'
+					+'; -o-transition-duration:'+options.transtime+'ms'});
+				refItems[i] = obj;
+			});
+			$(options.control).find('a').on('click', function() {
+				var t = $(this);
+				if(!cRunning) {
+					runCarousel({
+						rel:t.attr('rel'),
+						href:t.attr('href')
+					});
+				}
+				clearInterval(cInt);
+				return false;
+			});
+			refCont.find(':first-child').addClass('fade-in');
+			setActive();
+			cInt = setInterval(init, options.transtime + options.delay);
+		});
 		function init() {
 			cRunning = true;
 			runCarousel({rel:'next'});
@@ -66,32 +64,36 @@
 				default:
 					formula = 'episode';
 					break;
-			} // end switch
+			} 
 			var el = refItems[count(val.href)];
-			if (refCSSTrans) {
-				el
-					.removeClass(options.fadeOut).addClass(options.fadeIn)
-					.siblings().removeClass(options.fadeIn).addClass(options.fadeOut);
-				cRunning = false;	
-			} else {
-				el.fadeTo(options.transtime, 1, function() {
-					cRunning = false;
+				if (refCSSTrans) {
+					el
+						.removeClass(options.fadeOut).addClass(options.fadeIn)
+						.siblings().removeClass(options.fadeIn).addClass(options.fadeOut);
+					cRunning = false;	
+				} else {
+					el.fadeTo(options.transtime, 1, function() {
+						cRunning = false;
 				}).siblings().fadeTo(options.transtime, 0);
 			}
+			setActive(val);
+		}
+		function setActive(val) {
+			refControl.find('a').removeClass(options.active);
+			var t = refControl.find('.item-'+(cCount+1)+' a');
+			if(t.length>0) t.addClass(options.active);
 		}
 		function count(val) {
+			if(formula==='episode') cCount = $(val).index();
 			if(cCount === (refItems.length - 1)) {
-			    if(formula==='add') cCount = 0;
-			    if(formula==='subtract') cCount--;
-			    if(formula==='episode') cCount = $(val).index();
+				if(formula==='add') cCount = 0;
+				if(formula==='subtract') cCount--;
 			} else if(cCount === 0) {
-			    if(formula==='add') cCount++;
-			    if(formula==='subtract') cCount = refItems.length - 1;
-			    if(formula==='episode') cCount = $(val).index();
+				if(formula==='add') cCount++;
+				if(formula==='subtract') cCount = refItems.length - 1;
 			} else {
 				if(formula==='add') cCount++;
 				if(formula==='subtract') cCount--;
-				if(formula==='episode') cCount = $(val).index();
 			}
 			return cCount;
 		}
@@ -112,7 +114,7 @@
 								refControl.append('<li><a href="" rel="prev">Prev</a></li>');
 							} 
 							a = i+1;
-							refControl.append('<li><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
+							refControl.append('<li class="item-'+a+'"><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
 							if(i===refCont.children().length-1) {
 								refControl.append('<li><a href="" rel="next">Next</a></li>');
 							}
@@ -125,7 +127,7 @@
 								refControl = $(options.control);	
 							} 
 							a = i+1;
-							refControl.append('<li><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
+							refControl.append('<li class="item-'+a+'"><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
 						});
 						break;	
 				} 
@@ -134,6 +136,3 @@
 		}
 	};
 })(jQuery);
-
-
-$('#billboard').carouselFade();
