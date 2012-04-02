562353a496e4606365148bbbdc9c0a19ca3afaf1
Andreas Johansson <andreas@ibuypink.com>
2012-04-02 20:33:35 +0200

    Naming conventions, bug fix if only one item and id on prev/next.

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -7,27 +7,29 @@
 	$.fn.carouselFade = function(options) {
 		var defaults = {  
 			nav: 'full',
-			control: '#billboard-nav',
+			control: 'carousel-nav',
 			transdur: 1000,
 			fadeOut: 'fade-out',
 			fadeIn: 'fade-in',
 			active: 'active',
 			time: 500,
 			prev:'Prev',
-			next:'Next'
+			prevId:'carousel-prev',
+			next:'Next',
+			nextId:'carousel-next'
 		};  
 		var options = $.extend(defaults, options); 
 		var cCount = 0;
 		var cRunning = false; // if animation is running or not
-		var formula = 'add'; // default is add
+		var cFormula = 'add'; // default is add
 		var cInt; // carousel interval; autostart
-		var refItems = new Array(); // reference to each slide
 		var refCont = $(this); // container holding all slides
-		var refWidth = refCont.width();
+		var refItems = new Array(); // reference to each slide
+		var refContChildren = refCont.find('.item'); 
 		var refControl = setupNav(options.nav);
 		var refCSSTrans = Modernizr.csstransitions; // CSS Transitions availability in browser
 		return this.each(function() {  
-			refCont.find('.item').each(function(i) { 
+			refContChildren.each(function(i) { 
 			var obj = $(this)
 				.addClass('item-'+(i+1))
 				.attr({style:'transition-duration:'+options.transdur+'ms'
@@ -36,7 +38,7 @@
 					+'; -o-transition-duration:'+options.transdur+'ms'});
 				refItems[i] = obj;
 			});
-			$(options.control).find('a').on('click', function() {
+			$('#'+options.control).find('a').on('click', function() {
 				var t = $(this);
 				if(!cRunning) {
 					runCarousel({
@@ -47,7 +49,7 @@
 				clearInterval(cInt);
 				return false;
 			});
-			refCont.find(':first-child').addClass('fade-in');
+			refCont.find(':first-child').addClass(options.fadeIn);
 			setActive();
 			if(refItems.length>1) cInt = setInterval(init, options.transdur + options.time);
 		});
@@ -58,13 +60,13 @@
 		function runCarousel(val) {
 			switch(val.rel) {
 				case 'next':
-					formula = 'add';
+					cFormula = 'add';
 					break;
 				case 'prev':
-					formula = 'subtract';
+					cFormula = 'subtract';
 					break;
 				default:
-					formula = 'episode';
+					cFormula = 'episode';
 					break;
 			} 
 			var el = refItems[count(val.href)];
@@ -86,39 +88,39 @@
 			if(t.length>0) t.addClass(options.active);
 		}
 		function count(val) {
-			if(formula==='episode') cCount = $(val).index();
+			if(cFormula==='episode') cCount = $(val).index();
 			if(cCount === (refItems.length - 1)) {
-				if(formula==='add') cCount = 0;
-				if(formula==='subtract') cCount--;
+				if(cFormula==='add') cCount = 0;
+				if(cFormula==='subtract') cCount--;
 			} else if(cCount === 0) {
-				if(formula==='add') cCount++;
-				if(formula==='subtract') cCount = refItems.length - 1;
+				if(cFormula==='add') cCount++;
+				if(cFormula==='subtract') cCount = refItems.length - 1;
 			} else {
-				if(formula==='add') cCount++;
-				if(formula==='subtract') cCount--;
+				if(cFormula==='add') cCount++;
+				if(cFormula==='subtract') cCount--;
 			}
 			return cCount;
 		}
 		function setupNav(val) {
-			if($(options.control).length===0&&refItems.length>1) {
+			if($('#'+options.control).length===0&&refContChildren.length>1) {
 				switch(val) {
 					case 'carousel':
-						refCont.after('<ul id="'+options.control.replace('#', '')+'"></ul>');
-						refControl = $(options.control);	
-						refControl.append('<li><a href="#item" rel="prev">'+options.prev+'</a></li>');
-						refControl.append('<li><a href="#item" rel="next">'+options.next+'</a></li>');
+						refCont.after('<ul id="'+options.control+'"></ul>');
+						refControl = $('#'+options.control);	
+						refControl.append('<li id="'+options.prevId+'"><a href="javascript:;" rel="prev">'+options.prev+'</a></li>');
+						refControl.append('<li id="'+options.nextId+'"><a href="javascript:;" rel="next">'+options.next+'</a></li>');
 						break;
 					case 'full':
 						refCont.children().each(function(i) {
 							if(i===0) {
-								refCont.after('<ul id="'+options.control.replace('#', '')+'"></ul>');
-								refControl = $(options.control);	
-								refControl.append('<li><a href="" rel="prev">'+options.prev+'</a></li>');
+								refCont.after('<ul id="'+options.control+'"></ul>');
+								refControl = $('#'+options.control);	
+								refControl.append('<li id="'+options.prevId+'"><a href="" rel="prev">'+options.prev+'</a></li>');
 							} 
 							a = i+1;
 							refControl.append('<li class="item-'+a+'"><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
 							if(i===refCont.children().length-1) {
-								refControl.append('<li><a href="" rel="next">'+options.next+'</a></li>');
+								refControl.append('<li id="'+options.nextId+'"><a href="javascript:;" rel="next">'+options.next+'</a></li>');
 							}
 						});
 						break;	
@@ -126,7 +128,7 @@
 						refCont.children().each(function(i) {
 							if(i===0) {
 								refCont.after('<ul id="'+options.control.replace('#', '')+'"></ul>');
-								refControl = $(options.control);	
+								refControl = $('#'+options.control);	
 							} 
 							a = i+1;
 							refControl.append('<li class="item-'+a+'"><a href="#item-'+a+'" rel="episode">'+a+'</a></li>');
@@ -134,7 +136,7 @@
 						break;	
 				} 
 			}
-			return $(options.control);	
+			return $('#'+options.control);	
 		}
 	};
 })(jQuery);
